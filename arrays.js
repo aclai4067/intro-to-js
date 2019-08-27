c38754791b3c89d8358a4be3935493fdb0529920
Ashley Claiborne <languageisaliquid@gmail.com>
2019-08-26 19:09:11 -0500

    more array practice

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 console.log('arrays!');
 
+// Dino Array
+
 const dinosaurs = [
     {
         dinoType: 't-rex',
@@ -32,3 +34,63 @@ const buildDino = () => {
 };
 
 buildDino();
+
+// Challenge 1
+// Write a function that takes an array and an index and prints out that value
+
+const indexAndPrint = (arr, index) => {
+    console.log(arr[index]);
+};
+
+const randomArr= [1, 5, 8, 19, 25, 'dog', 'Bundaberg', false]
+
+indexAndPrint(randomArr, 2);
+
+// challenge 2
+// Write a function that takes an array of something and tells you if the name 'Greg' is in that array
+
+const somePeeps= ['Mandy', 'Janie', 'Tom', 'Ashley'];
+const otherPeeps= ['Callan', 'Michael', 'Greg', "Zoe"];
+
+// const findGreg = (name) => {
+//     for (i = 0; i < name.length; i++){
+//         if (name[i].toLowerCase() === 'greg') {
+//             console.log("It's Greg!");
+//         } else if (i = name.length && name[i].toLowerCase() !== 'greg'){
+//             console.log("no Greg :(")
+//         };
+//     };
+// };
+
+// findGreg(somePeeps);
+// findGreg(otherPeeps);
+
+const findGregBetterSolution = () => {
+    if (name.includes('Greg')) {
+        console.log("It's Greg!");
+    } else {
+        console.log("no Greg :(")
+    };
+};
+
+findGregBetterSolution(somePeeps);
+findGregBetterSolution(otherPeeps);
+
+// Challenge #3
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+// Console true if the given string is a palindrome. Otherwise, console false.
+
+const isItAPalindrome = (str) => {
+ const opposite = str.split('').reverse().join('');
+
+ if (str === opposite){
+    return true
+ } else {
+    return false
+ }
+};
+
+console.log(isItAPalindrome('cat'));
+console.log(isItAPalindrome('racecar'));
+console.log(isItAPalindrome('mom'));
+console.log(isItAPalindrome('bear'));
